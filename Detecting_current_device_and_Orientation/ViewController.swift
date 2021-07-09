7d997be8a7e73e2a12662ee7f34e475da34bce15
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-09 21:10:03 +0200

    Initial Commit

---
+++ Detecting_current_device_and_Orientation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detecting_current_device_and_Orientation
+//
+//  Created by Macbook on 09/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
