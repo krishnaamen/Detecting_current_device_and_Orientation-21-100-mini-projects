e3e9130554d977a4580216478bb498d3698669ee
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-10 19:12:09 +0200

    First commit

--- Detecting_current_device_and_Orientation/ViewController.swift
+++ Detecting_current_device_and_Orientation/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgView.image = #imageLiteral(resourceName: "o")
+whatIsMyDevice()
+        
     }
 
-
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape{
+            imgView.image = #imageLiteral(resourceName: "l")
+        }else{
+            imgView.image = #imageLiteral(resourceName: "p")
+        }
+        
+    }
+    
+    
+    
+    
+    func whatIsMyDevice(){
+        var phoneHeight = 0.00
+        if UIDevice().userInterfaceIdiom == .phone{
+            phoneHeight = Double(UIScreen.main.nativeBounds.height)
+            switch phoneHeight{
+            case 1136:
+                print("iphone 5 family")
+            case 1334:
+                print("iphone 6 or 6s or 7 or 8")
+            case 2436:
+                print("iphone x")
+            default:
+                print("Unknown")
+            }
+        }
+    }
 }
 
